02c3f737ccbf6e282ac0f803aee1af56021b03ad
cahaya12345 <bintangharyanto99@gmail.com>
2026-04-25 20:17:25 +0700

    fix home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
-import { CommonModule } from '@angular/common';
+import { IonicModule, Platform } from '@ionic/angular';
+import { CommonModule, Location } from '@angular/common';
+import { App } from '@capacitor/app';
 
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 
@@ -12,5 +13,23 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
   imports: [IonApp, IonRouterOutlet, CommonModule],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private platform: Platform, private location: Location) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.platform.backButton.subscribeWithPriority(10, (processNextHandler) => {
+        const currentPath = this.location.path();
+        
+        // Jika sedang di halaman utama, keluar dari aplikasi
+        if (currentPath === '/home' || currentPath === '') {
+          App.exitApp();
+        } else {
+          // Biarkan navigasi mundur standar Ionic berjalan untuk halaman lain
+          processNextHandler();
+        }
+      });
+    });
+  }
 }
